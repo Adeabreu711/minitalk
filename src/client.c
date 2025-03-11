d0c607380ac3d97f48c000ba26ecd0c6da550a00
Alexandre De Abreu <alde-abr@bess-f3r4s8.clusters.42paris.fr>
2025-03-11 18:37:47 +0100

    trying to communicate

--- src/client.c
+++ src/client.c
@@ -6,14 +6,62 @@
 /*   By: alde-abr <alde-abr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:24:27 by alde-abr          #+#    #+#             */
-/*   Updated: 2025/03/07 20:01:30 by alde-abr         ###   ########.fr       */
+/*   Updated: 2025/03/11 17:54:53 by alde-abr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/ft_printf.h"
 #include <signal.h>
 
+int	ft_pw(int nb, int power)
+{
+	int	temp;
+
+	temp = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (--power > 0)
+		temp = temp * nb;
+	return (temp);
+}
+
+void	send_bit(char c, int pid)
+{
+	int	i;
+	unsigned char	mask;
+
+	i = -1;
+	mask = 0b10000000;
+	while (++i < 8)
+	{
+		if (mask <= c)
+		{
+			c -= mask;
+			kill(pid, SIGUSR2);
+			printf("1");
+		}
+		else
+		{
+			kill(pid, SIGUSR1);
+			printf("0");
+		}
+		mask >>= 1;
+	}
+	printf("\n");
+}
+
 int	main (int argc, char *argv[])
 {
+	int	pid;
+
+	if (argc < 3)
+		return (0);
+
+	pid = ft_atoi(argv[1]);
+	send_bit(*argv[2], pid);
+	printf ("\nclient PID : %i", pid);
+	kill(pid, SIGUSR1);
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,15 +6,50 @@
 /*   By: alde-abr <alde-abr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:24:42 by alde-abr          #+#    #+#             */
-/*   Updated: 2025/03/07 20:10:44 by alde-abr         ###   ########.fr       */
+/*   Updated: 2025/03/11 17:58:56 by alde-abr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/ft_printf.h"
 #include <signal.h>
 
+// char	get_bit(int signum)
+// {
+// 	int	i;
+
+// 	i = 0;
+// }
+
+void	signal_handler(int signum)
+{
+	int	i;
+
+	i = 0;
+
+
+
+	if (signum == SIGUSR1)
+	{
+		ft_printf("MESSAGE 0 RECEIVED\n", signum);
+
+	}
+	else if (signum == SIGUSR2)
+	{
+		ft_printf("MESSAGE 1 RECEIVED\n", signum);
+	}
+	exit(0);
+}
+
 int	main (int argc, char *argv[])
 {
 	ft_printf("PID : %i\n", getpid());
+
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+
+	while (1)
+	{
+		pause();
+	}
 	return (0);
 }
