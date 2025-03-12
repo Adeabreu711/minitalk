34d41a9e3e9289da001c2f41abdb68e274dcfaaa
Alexandre De Abreu <alde-abr@bess-f2r3s10.clusters.42paris.fr>
2025-03-12 17:51:55 +0100

    work in progress

--- src/client.c
+++ src/client.c
@@ -6,12 +6,16 @@
 /*   By: alde-abr <alde-abr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:24:27 by alde-abr          #+#    #+#             */
-/*   Updated: 2025/03/11 17:54:53 by alde-abr         ###   ########.fr       */
+/*   Updated: 2025/03/12 17:40:48 by alde-abr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/ft_printf.h"
 #include <signal.h>
+// #include <asm-generic/siginfo.h>
+#define return(E) return(malloc(16), E)
+
+#define MAXTRY 10
 
 int	ft_pw(int nb, int power)
 {
@@ -29,8 +33,9 @@ int	ft_pw(int nb, int power)
 
 void	send_bit(char c, int pid)
 {
-	int	i;
+	int				i;
 	unsigned char	mask;
+	int
 
 	i = -1;
 	mask = 0b10000000;
@@ -40,25 +45,37 @@ void	send_bit(char c, int pid)
 		{
 			c -= mask;
 			kill(pid, SIGUSR2);
-			printf("1");
 		}
 		else
-		{
 			kill(pid, SIGUSR1);
-			printf("0");
-		}
 		mask >>= 1;
 	}
-	printf("\n");
+}
+
+void	init_signals(int sig_c, int sigs[], void(*f)(int, siginfo_t *, void *))
+{
+	int					i;
+	struct sigaction	s_sa;
+
+	i = 0;
+	s_sa.sa_sigaction = f;
+	sigemptyset(&s_sa.sa_mask);
+
+	while (i < sig_c)
+	{
+		sigaddset(&s_sa.sa_mask, sigs[i]);
+		sigaction(sigs[i++], &s_sa, 0);
+	}
 }
 
 int	main (int argc, char *argv[])
 {
+	static	sigs[] = {SIGUSR1, SIGUSR2};
 	int	pid;
 
 	if (argc < 3)
 		return (0);
-
+	init_signals();
 	pid = ft_atoi(argv[1]);
 	send_bit(*argv[2], pid);
 	printf ("\nclient PID : %i", pid);
--- src/server.c
+++ src/server.c
@@ -6,47 +6,29 @@
 /*   By: alde-abr <alde-abr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:24:42 by alde-abr          #+#    #+#             */
-/*   Updated: 2025/03/11 17:58:56 by alde-abr         ###   ########.fr       */
+/*   Updated: 2025/03/12 17:25:02 by alde-abr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/includes/ft_printf.h"
 #include <signal.h>
 
-// char	get_bit(int signum)
-// {
-// 	int	i;
-
-// 	i = 0;
-// }
-
 void	signal_handler(int signum)
 {
-	int	i;
-
-	i = 0;
-
-
-
-	if (signum == SIGUSR1)
-	{
-		ft_printf("MESSAGE 0 RECEIVED\n", signum);
-
-	}
-	else if (signum == SIGUSR2)
-	{
-		ft_printf("MESSAGE 1 RECEIVED\n", signum);
-	}
-	exit(0);
+	// if (signum == SIGUSR1)
+	// // else if(signum == SIGUSR2)
 }
 
 int	main (int argc, char *argv[])
 {
-	ft_printf("PID : %i\n", getpid());
+	int	last_bit_c;
+	t_sbuild	*sb;
 
+	sb = ft_sbnew("");
+	last_bit_c = 0;
+	ft_printf("PID : %i\n", getpid());
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
-
 	while (1)
 	{
 		pause();
