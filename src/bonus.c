a5eba88a93afdbc5e4e33c8503a5f8ffa2b52277
Alexandre De Abreu <alde-abr@bess-f4r5s3.clusters.42paris.fr>
2025-03-18 20:11:51 +0100

    started to adding colors

--- src/bonus.c
+++ src/bonus.c
@@ -1,23 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   bonus.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alde-abr <alde-abr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/17 18:16:23 by alde-abr          #+#    #+#             */
-/*   Updated: 2025/03/18 18:58:35 by alde-abr         ###   ########.fr       */
+/*   Created: 2025/03/18 18:36:07 by alde-abr          #+#    #+#             */
+/*   Updated: 2025/03/18 18:36:07 by alde-abr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
-
-# include <signal.h>
-# include "../libft/includes/ft_printf.h"
-# include "../libft/includes/ft_string_builder.h"
-# include "../libft/includes/color.h"
-
-void	init_signals(int sig_c, int sigs[], void(*f)(int, siginfo_t *, void *));
-
-#endif
--- src/server.c
+++ src/server.c
@@ -6,11 +6,16 @@
 /*   By: alde-abr <alde-abr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 18:24:42 by alde-abr          #+#    #+#             */
-/*   Updated: 2025/03/17 22:27:56 by alde-abr         ###   ########.fr       */
+/*   Updated: 2025/03/18 19:22:03 by alde-abr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
+
+#define SHOW_PID "%sPID : %i\n\e[0m"
+#define MSG_RECEIVED "%smessage received from \e[0m%sPID[%i]!\n\n\e[0m"
+#define PRINT_MSG "%smessage :\e[0m%s [%s]\n\n\e[0m"
+#define SEND_RECEPTION "%ssending reception...\n\e[0m"
 
 int	compare_commands(char *str, int pid)
 {
@@ -25,10 +30,10 @@ void	close_reception(t_sbuild **sb, int pid)
 	ft_sbclear(sb);
 	if (!str)
 		return ;
-	ft_printf("message received from PID[%i]!\n", pid);
+	ft_printf(MSG_RECEIVED, GREEN, GREEN_B, pid);
 	// if (compare_commands(str, pid))
-	ft_printf("%s\n", str);
-	ft_printf("sending reception...\n", pid);
+	ft_printf(PRINT_MSG, GREY, GREY_B ,str);
+	ft_printf(SEND_RECEPTION, GREY, pid);
 	if (kill(pid, SIGUSR2))
 		ft_printf("aborting : cannot send reception to PID[%i]\n", pid);
 	else
@@ -53,7 +58,6 @@ void	sig_handler(int sig, siginfo_t *siginfo, void *empty)
 	if (count < 8)
 		return ;
 	ft_sb_addstr(&sb, &oct, 1);
-	printf("char : %i\n", oct);
 	if (!oct)
 		close_reception(&sb, siginfo->si_pid);
 	oct = 0b0;
@@ -66,7 +70,8 @@ int	main (int argc, char *argv[])
 	t_sbuild	*sb;
 
 	sb = ft_sbnew("");
-	ft_printf("PID : %i\n",  getpid());
+	printf("\033[1;31m");
+	ft_printf(SHOW_PID, GREEN_B, getpid());
 	init_signals(2, sigs, sig_handler);
 	while (1)
 		pause();
